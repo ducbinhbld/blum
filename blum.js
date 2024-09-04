d436c1881c13041f193db7b176e85b4d0c58f203
TonyPham-Dev <phamvancong300701@gmail.com>
2024-09-04 15:48:49 +0700

    add iframe token

--- blum.js
+++ blum.js
@@ -1,8 +1,11 @@
 const axios = require('axios');
 const colors = require('colors');
 const fs = require('fs');
+const path = require('path');
 const {DateTime} = require("luxon");
 const users = JSON.parse(fs.readFileSync('blum.json', 'utf8'));
+
+const IFRAME = "https://telegram.blum.codes/#tgWebAppData=query_id%3DAAHSSfZJAgAAANJJ9kkA9t8v%26user%3D%257B%2522id%2522%253A5535844818%252C%2522first_name%2522%253A%2522Tony%2522%252C%2522last_name%2522%253A%2522Pham%2522%252C%2522username%2522%253A%2522TonyPham01%2522%252C%2522language_code%2522%253A%2522en%2522%252C%2522allows_write_to_pm%2522%253Atrue%257D%26auth_date%3D1725433916%26hash%3Da5577ea4df778ef493446c7e332a0b634f19e87bbb56956a164a5f1cd9ca38fe&tgWebAppVersion=7.8&tgWebAppPlatform=weba&tgWebAppThemeParams=%7B%22bg_color%22%3A%22%23212121%22%2C%22text_color%22%3A%22%23ffffff%22%2C%22hint_color%22%3A%22%23aaaaaa%22%2C%22link_color%22%3A%22%238774e1%22%2C%22button_color%22%3A%22%238774e1%22%2C%22button_text_color%22%3A%22%23ffffff%22%2C%22secondary_bg_color%22%3A%22%230f0f0f%22%2C%22header_bg_color%22%3A%22%23212121%22%2C%22accent_text_color%22%3A%22%238774e1%22%2C%22section_bg_color%22%3A%22%23212121%22%2C%22section_header_text_color%22%3A%22%23aaaaaa%22%2C%22subtitle_text_color%22%3A%22%23aaaaaa%22%2C%22destructive_text_color%22%3A%22%23e53935%22%7D"
 
 async function getHeaders(user) {
   return {
@@ -234,7 +237,7 @@ async function startTask(user, taskId) {
     const response = await axios.post(`https://game-domain.blum.codes/api/v1/tasks/${taskId}/start`, {}, {headers: headers});
     return response.data;
   } catch (error) {
-     console.log(`Không thể bắt đầu nhiệm vụ ${taskId}: ${error.message}`);
+    //  console.log(`Không thể bắt đầu nhiệm vụ ${taskId}: ${error.message}`);
     return null;
   }
 }
@@ -267,11 +270,147 @@ async function joinTribe(user, tribeId) {
     return false;
   }
 }
+async function processToken() {
+  let decodedData = "";
+  try {
+      const parsedUrl = new URL(IFRAME);
+      
+      const fragment = parsedUrl.hash.substring(1);
+      console.log(fragment);
+      // Extract tgWebAppData parameter
+      const params = new URLSearchParams(fragment);
+      const tgWebAppData = params.get('tgWebAppData');
+
+      // Decode the tgWebAppData value
+      decodedData = decodeURIComponent(tgWebAppData);
+      const token = await getNewToken(decodedData);
+      const userInfo = await getUserInfos(token);
+
+      updateBlumJson(token, userInfo.username);
+      return true
+  } catch (e) {
+      console.log("error", e);
+  }
+}
+
+async function getNewToken(queryId) {
+  const headers = {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/json",
+    "origin": "https://telegram.blum.codes",
+    "referer": "https://telegram.blum.codes/"
+  };
+
+  const data = JSON.stringify({ query: queryId });
+  const url = "https://gateway.blum.codes/v1/auth/provider/PROVIDER_TELEGRAM_MINI_APP";
+
+  for (let attempt = 0; attempt < 3; attempt++) {
+      console.log("Getting token...");
+      try {
+          const requestConfig = {
+              method: 'post',
+              url: url,
+              data: data,
+              headers: headers
+          };
+          const response = await axios(requestConfig);
+
+          if (response.status === 200) {
+              console.log("Token successfully created");
+              return response.data.token.refresh;
+          } else {
+              console.log(response.data);
+              console.log(`Failed to get token, attempt ${attempt + 1}`);
+          }
+      } catch (error) {
+          console.log(`Failed to get token, attempt ${attempt + 1}`);
+          console.log(error);
+      }
+  }
+  return null;
+}
+
+function updateBlumJson(token, username) {
+  try {
+      const filePath = path.join(__dirname, './blum.json');
+      const data = fs.readFileSync(filePath, 'utf8');
+      const jsonData = JSON.parse(data);
+
+      let userFound = false;
+      for (const item of jsonData) {
+          if (item.user === username) {
+              item.access = token;
+              userFound = true;
+              break;
+          }
+      }
+
+      if (!userFound) {
+          jsonData.push({
+              access: token,
+              refresh: "",
+              user: username
+          });
+      }
+
+      fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2), 'utf8');
+      console.log('File updated successfully');
+  } catch (error) {
+      console.error('Error updating file:', error);
+  }
+}
+
+
+async function getUserInfos(token) {
+  const headers = {
+    'Authorization': `Bearer ${token}`,
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'en-US,en;q=0.9',
+    'origin': 'https://telegram.blum.codes',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0'
+  };
+
+  try {
+      const requestConfig = {
+          method: 'get',
+          url: 'https://gateway.blum.codes/v1/user/me',
+          headers,
+      };
+      const response = await axios(requestConfig);
+
+      if (response.status === 200) {
+          return response.data;
+      } else {
+          const result = response.data;
+
+          if (result.message === 'Token is invalid') {
+              console.log("Invalid token, getting a new token...");
+              const newToken = await getNewToken("", proxy);
+              if (newToken) {
+                  console.log("New token obtained, trying again...");
+                  return getUserInfo(newToken, proxy);
+              } else {
+                  console.log("Failed to get a new token.");
+                  return null;
+              }
+          } else {
+              console.log("Failed to get user information");
+              return null;
+          }
+      }
+  } catch (e) {
+      // console.log(e);
+      return null;
+  }
+}
 
 // Sử dụng các hàm đã định nghĩa ở trên
 (async () => {
   console.log(`============== BLUM ==============`);
   console.log(`============== ${DateTime.now().toString()} ==============`);
+  await processToken()
+  await new Promise(r => setTimeout(r, 2000));
   for (let i = 0; i < users.length; i++) {
     console.log(`Bắt đầu tài khoản ${users[i].user ?? ''}`)
     let user = users[i];
@@ -285,15 +424,10 @@ async function joinTribe(user, tribeId) {
     await checkBalanceFriend(user);
     await claimBalanceFriend(user);
     await dailyReward(user);
+    const tribeId = 'b372af40-6e97-4782-b70d-4fc7ea435022';
+    await joinTribe(user, tribeId);
     if (balance && typeof balance.farming === 'undefined') {
       await startFarming(user);
-      const claimRes = await claimBalance(user);
-      if (claimRes) {
-        const randTime1 = Math.floor(Math.random() * 3) + 1;
-        await new Promise(resolve => setTimeout(resolve, (2 + randTime1) * 1000));
-        const tribeId = 'b372af40-6e97-4782-b70d-4fc7ea435022';
-        await joinTribe(user, tribeId);
-      }
     }
 
     const dataTask = await getTasks(user);
